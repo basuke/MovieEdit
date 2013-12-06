d900ae9ebb60ef05dc6aceb29a0403bce45b1bc5
Basuke Suzuki <basuke@kinoma.com>
2013-12-06 14:27:08 -0800

    Initial Commit

---
+++ MovieEditor/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MovieEditor
+//
+//  Created by Yosuke Suzuki on 2013/12/06.
+//  Copyright (c) 2013年 Basuke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
